dfa07940e62d2ef61ec67191cc55842dbd1d70b4
Ivan Debono <ivan.debono@in2p3.fr>
2018-11-21 15:28:17 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -42,6 +42,7 @@ setup(name='MCEvidence',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
       ],
       keywords=['MCMC', 'Evidence', 'bayesian evidence', 'marginal likelihood']
       )
